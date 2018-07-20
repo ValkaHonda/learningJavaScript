214576a3bad3de0383ab8c8b15accccc755df32e
Lenny <valentin805@gmail.com>
2018-07-20 16:14:26 +0300

    add action listener to the buttons of catch.html

---
+++ Asynchronous programming/FisherGame/catch.js
@@ -0,0 +1,18 @@
+function attachEvents(){
+    $('.update').click(update);
+    $('.delete').click(del);
+    $('.load').click(load);
+    $('.add').click(add);
+}
+function update(){
+    console.log('update function');
+}
+function del(){
+    console.log('delete function');
+}
+function load(){
+    console.log('load function');
+}
+function add(){
+    console.log('add function');
+}
