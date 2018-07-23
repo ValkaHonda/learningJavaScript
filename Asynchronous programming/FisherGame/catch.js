8038a67a77c5017169f48dc80a0e94600a1253bd
Lenny <valentin805@gmail.com>
2018-07-23 09:21:06 +0300

    Add load angler functionality

--- Asynchronous programming/FisherGame/catch.js
+++ Asynchronous programming/FisherGame/catch.js
@@ -1,3 +1,7 @@
+const URL = 'https://baas.kinvey.com/appdata/kid_Skz-pJ0Qm/messages';
+const USER_NAME = 'pingvin_8@mail.bg';
+PASSWORD = '883368107754';
+
 function attachEvents(){
     $('.update').click(update);
     $('.delete').click(del);
@@ -11,9 +15,50 @@ function del(){
     console.log('delete function');
 }
 function load(){
-    console.log('load function');
     $('#catches').empty();
+    $.ajax({
+        method: 'get',
+        url: URL,
+        headers: {
+            "Authorization": "Basic " + btoa(USER_NAME + ":" + PASSWORD)
+        },
+        success: function(respond){
+            createAnglerFrame();
+            for(let i = 0; i < respond.length; i++){
+                let angler = respond[i];
+                addAnglerView(angler);
+            }
+        },
+        error: function(error){
+            console.log(error);
+        }
+    });
 }
 function add(){
     console.log('add function');
+}
+
+function addAnglerView(angler){
+    $('#catches').append(`<div class="catch" data-id="${angler._id}">
+    <label>Angler</label>
+    <input type="text" class="angler" value="${angler.angler}"/>
+    <label>Weight</label>
+    <input type="number" class="weight" value="${angler.weight}"/>
+    <label>Species</label>
+    <input type="text" class="species" value="${angler.species}"/>
+    <label>Location</label>
+    <input type="text" class="location" value="${angler.location}"/>
+    <label>Bait</label>
+    <input type="text" class="bait" value="${angler.bait}"/>
+    <label>Capture Time</label>
+    <input type="number" class="captureTime" value="${angler.captureTime}"/>
+    <button class="update">Update</button>
+    <button class="delete">Delete</button>
+    </div>`
+    );
+}
+function createAnglerFrame(){
+    $('#heading').after(`<fieldset id="main"><legend>Catches</legend>
+    <div id="catches">
+    </div></fieldset>`);
 }
